7050cb1482886f62755381f2037ab9abe5f5dbef
BredyByte <correodemimobil@gmail.com>
2024-01-04 19:27:33 +0100

    feat: check map name, check map texture is created
    Is created new function that checks map/file's extension passed like second
    argument.
    Is created function that checks texture imgs.

--- src/init.c
+++ src/init.c
@@ -6,20 +6,24 @@
 /*   By: dbredykh <dbredykh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 12:42:08 by dbredykh          #+#    #+#             */
-/*   Updated: 2024/01/03 12:58:17 by dbredykh         ###   ########.fr       */
+/*   Updated: 2024/01/04 19:16:40 by dbredykh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-
 int	main(int argc, char **argv)
 {
 	t_info	*info;
+	int		fd;
 
-	(void) argv;
 	info = (t_info *)malloc(sizeof(t_info));
 	if (argc < 2)
 		put_usage(info);
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0 || check_filename(argv[1]))
+		return (put_error("Error: map\n"), 0);
+	if (!check_map(info, fd))
+		return (free(info), 1);
 	return (0);
 }
