a892df09cf2e8ac41b9ad67176df517418051a7c
BredyByte <correodemimobil@gmail.com>
2024-01-11 15:47:17 +0100

    fix: get_error func fixed

--- src/get_error_message.c
+++ src/get_error_message.c
@@ -6,7 +6,7 @@
 /*   By: dbredykh <dbredykh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 09:31:52 by dbredykh          #+#    #+#             */
-/*   Updated: 2024/01/11 10:04:10 by dbredykh         ###   ########.fr       */
+/*   Updated: 2024/01/11 15:46:04 by dbredykh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ const char	*get_error_message(enum e_ErrorCode code)
 		return ("Error: Can't allocate memory");
 	else if (code == E_OBLIG_DATA)
 		return ("Error: Wrong obligatory data");
-	else if (E_INVALID_FILE)
+	else if (code == E_INVALID_FILE)
 		return ("Error: Wrong passed file");
-	else if (E_INVALID_MAP)
+	else if (code == E_INVALID_MAP)
 		return ("Error: Invalid map format");
 	else
 		return ("Unknown error");
