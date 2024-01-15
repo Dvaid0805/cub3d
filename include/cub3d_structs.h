bb917ac78e31084a7bd2976ba960e533974e7fec
pablovilchez <pablovilchez.r@gmail.com>
2024-01-15 14:29:01 +0100

    test

--- include/cub3d_structs.h
+++ include/cub3d_structs.h
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 19:55:58 by pvilchez          #+#    #+#             */
-/*   Updated: 2024/01/12 22:06:32 by pvilchez         ###   ########.fr       */
+/*   Updated: 2024/01/15 13:03:08 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,9 @@ typedef struct s_ray
 {
 	double		dir_x;
 	double		dir_y;
-	double		plane_x;
-	double		plane_y;
-	double		camera_x;
-	double		ray_dir_x;
-	double		ray_dir_y;
-	int			map_x;
-	int			map_y;
-	double		side_dist_x;
-	double		side_dist_y;
-	double		delta_dist_x;
+	double		draw_dist;
 	double		delta_dist_y;
-	double		perp_wall_dist;
-	int			step_x;
-	int			step_y;
-	int			hit;
-	int			side;
-	int			line_height;
-	int			draw_start;
-	int			draw_end;
-	double		wall_x;
-	int			tex_x;
-	int			tex_y;
-	double		step;
-	double		tex_pos;
+	
 }				t_ray;
 
 typedef struct s_info
